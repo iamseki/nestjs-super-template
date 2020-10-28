125f8360f2856d8b6f403f72140c8c90414c748d
iamseki <chrisyuri_19@hotmail.com>
2020-10-28 16:52:26 -0300

    Add User and Device domain nest template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { DeviceModule } from './modules/device/device.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DeviceModule],
   controllers: [AppController],
   providers: [AppService],
 })
