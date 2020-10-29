2ca9227ac9e6bca9938b66a96f47080087f9b599
iamseki <chrisyuri_19@hotmail.com>
2020-10-29 08:31:32 -0300

    Remove unnecessary nestjs template files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { DeviceModule } from './modules/device/device.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as ormconfig from './database/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), UserModule, DeviceModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(ormconfig), UserModule, DeviceModule]
 })
 export class AppModule {}
